aa2355e4a6d15d33f0834703f90df3600a976239
Bruno Henrique Laviso Adorne <bruno.adorne@ikatec.com.br>
2022-05-09 19:51:33 -0300

    feat: criado Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <header>
+      <ul>
+        <li>A Yeti</li>
+        <li>O Que Fazemos</li>
+        <li>Contato</li>
+        <li>Maquina de Criativos</li>
+      </ul>
+    </header>
+  )
+}
