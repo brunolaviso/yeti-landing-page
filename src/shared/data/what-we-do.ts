9228c9f166f3da40c8bf47674eb0573791d35540
Bruno Henrique Laviso Adorne <bruno.adorne@ikatec.com.br>
2022-05-09 19:52:42 -0300

    feat: inserido array com seção de "o que fazemos"

---
+++ src/shared/data/what-we-do.ts
@@ -0,0 +1,73 @@
+export const data = [
+  {
+    id: 0,
+    title: 'Comunicação ON e OFF',
+    content: `A Yeti é especialista na criação tanto de marketing online,
+    quanto offline. Desenvolvemos toda a identidade visual e o design de
+    todas as peças necessárias, no meio on e offline. Além disso, criamos um
+    plano de comunicação e conteúdo que remete aos valores e objetivos da sua marca.`
+  },
+  {
+    id: 1,
+    title: 'Social Media Full Service',
+    content: `Planejamento de comunicação e análise de canais;
+    Produção de conteúdo multimídia;
+    Shooting: produção audio-visual (fotos e vídeos);
+    Gestão de múltiplos canais:`
+  },
+  {
+    id: 2,
+    title: 'Branding',
+    content: `Para ser disruptiva e se transformar digitalmente,
+    uma empresa deve construir uma marca admirável,
+    para assim atrair os melhores talentos e clientes.
+    Para inovar, a sua empresa precisa, antes de tudo, ser sexy!`
+  },
+  {
+    id: 3,
+    title: 'Growth',
+    content: `Também podemos chamar de estratégia de crescimento,
+    ou simplesmente growth. Reunimos dados precisos,
+    tecnologia de ponta para entregar a melhor performance de marketing e criar grandes cases.
+    Construir uma operação e garantir a otimização de cada parte da sua
+    estratégia de marketing é o que irá levar sua empresa para o próximo nível.`
+  },
+  {
+    id: 4,
+    title: 'Growth',
+    content: `Também podemos chamar de estratégia de crescimento,
+    ou simplesmente growth. Reunimos dados precisos,
+    tecnologia de ponta para entregar a melhor performance de marketing e criar grandes cases.
+    Construir uma operação e garantir a otimização de cada parte da sua
+    estratégia de marketing é o que irá levar sua empresa para o próximo nível.`
+  },
+  {
+    id: 5,
+    title: 'Criação de Sites',
+    content: `Nós não criamos apenas simples sites, criamos uma identidade na INTERNET.
+    Nossos SITES são todos responsiveis, com painel administrativo, integrado a
+    ferramenta de análise de desempenho Google Analytics.`
+  },
+  {
+    id: 6,
+    title: 'Automações',
+    content: `Inbound, marketing de atração, automação de marketing… São muitos nomes para a
+    principal estratégia de empresas que buscam escalar seus negócios e estabelecer um
+    relacionamento que gere valor e amplie a conversão em vendas.`
+  },
+  {
+    id: 7,
+    title: 'Consultoria',
+    content: `O principal propósito da nossa consultoria é estabelecer um processo comercial 
+    estruturado, capaz de encontrar o cliente ideal, estabelecer um contato e gerar valor e interesse em uma proposta.`
+  },
+  {
+    id: 8,
+    title: 'Máquina de Criativos',
+    content: `Em um mercado tão competitivo, o criativo pode fazer toda a diferença na hora de
+    atrair a atenção do seu público e aumentar a taxa de conversão dos seus anúncios.
+    Seu anúncio precisa atrair a atenção da sua audiência; despertar interesse do público,
+    mostrando o que seu produto tem a oferecer; estimular o desejo, indicando qual tipo de
+    problema seu produto/serviço resolve e qual necessidade ele satisfaz. Essas ações implicam numa ação mais produtiva para sua marca.`
+  },
+]
---
+++ src/shared/types/what-we-do.ts
@@ -0,0 +1,5 @@
+export interface IWhatWeDo {
+  id: number,
+  title: string,
+  content: string
+}
